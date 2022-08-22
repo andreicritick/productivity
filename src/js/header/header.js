207ed78848d206ab0b596d5986feebd94f9a51d5
Andrey <proflinervk@gmail.com>
2022-08-23 01:58:31 +0300

    Feature/header (#3)
    * added header(desktop)
    
    * adeed favicon,mobile header,form and JS for header
    
    * fixed some bugs

---
+++ src/js/header/header.js
@@ -0,0 +1,21 @@
+document.addEventListener( 'DOMContentLoaded', () => {
+    'use strict'
+
+    window.addEventListener('scroll', headerScroll )
+})
+
+const headerScroll = () => {
+    const scrollTop = window.scrollY
+    const header = document.querySelector( '.header' )
+
+    if  ( ! header ) return
+
+    if ( scrollTop > 0 ) {
+        if ( ! header.classList.contains( 'scrolled' ) )
+            header.classList.add( 'scrolled' )
+
+    }   else {
+        if ( header.classList.contains( 'scrolled' ) )
+            header.classList.remove( 'scrolled' )
+    }
+}
